5dfeb72e21121ff0924e9caba04107338e3551d4
Sami Mäkelä <mrsmkl@gmail.com>
2016-11-26 19:41:59 +0200

    thinking about program length

--- compaux.thy
+++ compaux.thy
@@ -1287,11 +1287,24 @@ apply(auto)
 fun  program_list_of_lst  :: "inst list \<Rightarrow> inst list"  where 
  " program_list_of_lst [] = []"
 |" program_list_of_lst (Stack (PUSH_N bytes) # rest) =
-   map(\<lambda>x. Unknown x) bytes @ 
-   [Stack (PUSH_N bytes)] @
+    [Stack (PUSH_N bytes)] @
+    map(\<lambda>x. Unknown x) bytes @ 
    program_list_of_lst rest"
 |" program_list_of_lst (Annotation _  # rest) = program_list_of_lst rest"
 |" program_list_of_lst (i # rest) = i # program_list_of_lst rest" 
+
+theorem program_list_append :
+  "program_list_of_lst (a@b) =
+   program_list_of_lst a @ program_list_of_lst b"
+apply(induction a rule:program_list_of_lst.induct)
+apply(auto)
+done
+
+theorem program_list_length :
+  "length (program_list_of_lst [a]) = program_size [a]"
+apply(auto simp:program_size.simps)
+apply(induction a rule:inst_code.induct)
+apply(auto)
 
 theorem content_small_aux_bytes [simp] :
   "k < pos \<Longrightarrow>
@@ -1375,6 +1388,15 @@ apply(subst unfold_update)
 apply(auto)
 done
 
+theorem combine_some : "m2 n = Some x \<Longrightarrow> Some x = (m1 ++ m2) n"
+apply(auto)
+done
+
+theorem combine_none :
+   "m2 x = None \<Longrightarrow> m3 x = m1 x \<Longrightarrow> m3 x = (m1 ++ m2) x"
+apply(auto)
+by (simp add: map_add_def)
+
 theorem program_list_bytes [simp] :
   "store_byte_list_in_program pos bytes m =
    m ++ make_unknown pos bytes"
@@ -1405,10 +1427,134 @@ apply(subst make_unknown_lemma)
 apply(auto simp:unknown_union)
 done
 
+theorem index_one [simp] :
+  "0 < n \<Longrightarrow> index (x # lst) n = index lst (n-1)"
+apply(cases n)
+apply(auto)
+done
+
+theorem lemma_2 [simp] :
+  "0 < n \<Longrightarrow>
+   (\<And>t n. index (program_list_of_lst lst) n =
+   program_content_of_lst t lst (n + t)) \<Longrightarrow>
+   index (program_list_of_lst lst) (n-1) =
+   program_content_of_lst (Suc t) lst (n + t)"
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+done
+
+theorem index_append_small [simp] :
+  "n < length a \<Longrightarrow> index (a @ b) n = index a n"
+apply(auto)
+by (simp add: nth_append)
+
+theorem index_append_large1 [simp] :
+  "index (a @ b) (n+length a) = index b n"
+apply(induction a arbitrary:n b)
+apply(auto)
+done
+
+theorem index_append_large [simp] :
+  "n \<ge> length a \<Longrightarrow> index (a @ b) n = index b (n-length a)"
+by (metis index_append_large1 ordered_cancel_comm_monoid_diff_class.diff_add)
+
+fun get_stack :: "inst \<Rightarrow> stack_inst" where
+"get_stack (Stack s) = s"
+
+fun get_push_bytes :: "inst \<Rightarrow> byte list" where
+"get_push_bytes (Stack (PUSH_N s)) = s"
+
+theorem get_unknown [simp] :
+  "n < length lst \<Longrightarrow>
+   make_unknown t lst (n+t) = Some (Unknown (lst!n))"
+apply(induction lst arbitrary:n t)
+apply(simp)
+apply(auto)
+proof -
+  fix lsta :: "8 word list" and na :: nat and ta :: nat
+  assume a1: "na < Suc (length lsta)"
+  assume a2: "0 < na"
+  assume a3: "\<And>n t. n < length lsta \<Longrightarrow> make_unknown t lsta (n + t) = Some (Unknown (lsta ! n))"
+  obtain nn :: "nat \<Rightarrow> nat \<Rightarrow> nat" where
+    "\<forall>x0 x1. (\<exists>v2. x1 = Suc v2 \<and> v2 < x0) = (x1 = Suc (nn x0 x1) \<and> nn x0 x1 < x0)"
+    by moura
+  then have f4: "na = Suc (nn (length lsta) na) \<and> nn (length lsta) na < length lsta"
+    using a2 a1 less_Suc_eq_0_disj by auto
+  then have f5: "na + ta = nn (length lsta) na + Suc ta"
+    by (metis add_Suc_shift)
+  have "nn (length lsta) na = na - Suc 0"
+    using f4 diff_Suc_1 by presburger
+  then show "make_unknown (Suc ta) lsta (na + ta) = Some (Unknown (lsta ! (na - Suc 0)))"
+    using f5 f4 a3 by presburger
+qed
+
+theorem get_unknown2 [simp] :
+  "n \<ge> length lst \<Longrightarrow>
+   make_unknown t lst (n+t) = None"
+apply(induction lst arbitrary:n t)
+apply(simp)
+apply(auto)
+by (metis (mono_tags, lifting) Suc_leD Suc_le_D add_Suc_shift le_SucE)
+
+theorem lemma_3 :
+    "(\<And>t n.
+           index (program_list_of_lst lst) n =
+           program_content_of_lst t lst (n + t)) \<Longrightarrow>
+       n \<ge> x2 + 1 \<Longrightarrow>
+       index (program_list_of_lst lst) (n - Suc x2) =
+       program_content_of_lst (t + Suc x2) lst (n+t)"
+apply(auto)
+by (smt ab_semigroup_add_class.add_ac(1) add.commute add_Suc_right le_add_diff_inverse2)
+
+theorem lemma_1 :
+   "(\<And>t n.
+           index (program_list_of_lst lst) n =
+           program_content_of_lst t lst (n + t)) \<Longrightarrow>
+       index (program_list_of_lst (a # lst)) n =
+       program_content_of_lst t (a # lst) (n + t)"
+apply(cases a)
+apply(auto)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+defer
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply(cases "get_stack a")
+apply(auto)
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+defer
+apply (metis One_nat_def add_Suc_shift diff_Suc_1 less_imp_Suc_add)
+apply(cases "n = 0")
+apply(simp add:nth_append make_unknown_lemma)
+apply(simp add:nth_append make_unknown_lemma)
+apply(cases "n-1 < length (get_push_bytes a)")
+apply(simp add:nth_append make_unknown_lemma)
+apply(rule combine_some)
+apply (metis Suc_pred add_Suc_shift get_unknown)
+apply(simp add:nth_append make_unknown_lemma) (* n-1 \<ge> length x2 *)
+apply(rule combine_none)
+apply (metis Suc_pred add_Suc_shift get_unknown2 linorder_not_less)
+using lemma_3
+apply (fastforce)
+done
+
+theorem program_list_content_eq :
+  "index (program_list_of_lst lst) n =
+    program_content_of_lst t lst (n+t)"
+apply(induction lst arbitrary:t n)
+apply(auto simp:lemma_1)
+done
+
 theorem program_list_content :
   "set (program_list_of_lst lst) = ran (program_content_of_lst 0 lst)"
 apply(induction lst rule:program_content_of_lst.induct)
-apply(simp)
 apply(simp)
 defer
 apply(simp)
